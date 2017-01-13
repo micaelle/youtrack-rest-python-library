72e67cd954697ebb703f5968d0f52ec007306061
mfedchenko <michael.fedchenko@fastdev.se>
2017-01-13 23:46:21 +0500

    Mapping Feedback state to Opened

--- python/redmine/mapping.py
+++ python/redmine/mapping.py
@@ -35,6 +35,7 @@ class Mapping(object):
             'Open'          : 'Opened',
             'In progress'   : 'Work in progress',
             'Done'          : 'Fixed',
+            'Feedback'      : 'Open'
         },
         'Priority': {
             'Blocking'  : 'Blocking',
